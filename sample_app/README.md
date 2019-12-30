00293651e70cf3c2259e2c6dbcc1bfed19e787f0
Ubuntu <ubuntu@ip-172-31-86-228.ec2.internal>
2019-12-30 09:18:08 +0000

    Finished

--- sample_app/README.md
+++ sample_app/README.md
@@ -1,32 +1,24 @@
-# Ruby on Rails Tutorial sample application
+# README
 
-This is the sample application for
-[*Ruby on Rails Tutorial:
-Learn Web Development with Rails*](http://www.railstutorial.org/)
-by [Michael Hartl](http://www.michaelhartl.com/).
+This README would normally document whatever steps are necessary to get the
+application up and running.
 
-## Getting started
+Things you may want to cover:
 
-To get started with the app, clone the repo and then install the needed gems:
+* Ruby version
 
-```
-$ bundle install --without production
-```
+* System dependencies
 
-Next, migrate the database:
+* Configuration
 
-```
-$ rails db:migrate
-```
+* Database creation
 
-Finally, run the test suite to verify that everything is working correctly:
+* Database initialization
 
-```
-$ rails test
-```
+* How to run the test suite
 
-If the test suite passes, you'll be ready to run the app in a local server:
+* Services (job queues, cache servers, search engines, etc.)
 
-```
-$ rails s
-```
+* Deployment instructions
+
+* ...
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
-  root   'static_pages#home'
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  get 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :relationships,       only: [:create, :destroy]
+  resources :microposts, only:[:create, :destroy]
 end
